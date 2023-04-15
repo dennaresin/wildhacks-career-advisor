b33796c9c6df1ce49ac1d3b51c71d9f015b4ffb0
Weff24 <jwny123@gmail.com>
2023-04-15 14:10:10 -0500

    Set up firebase

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,29 @@
 import { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { initializeApp } from "firebase/app";
 
 const App = () => {
   const [count, setCount] = useState(0);
+
+  
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyARv6bbK6G7hHXZMt0Xy0-jEOGAFSHAk3k",
+    authDomain: "mentat-hacks.firebaseapp.com",
+    projectId: "mentat-hacks",
+    storageBucket: "mentat-hacks.appspot.com",
+    messagingSenderId: "805921278932",
+    appId: "1:805921278932:web:a1b5fd223cd5c932e22441"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+
+
 
   return (
     <div className="App">
